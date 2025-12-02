eabd0af55bf691c6191bd09f5c7bfc81f6daefb1
hakonovrebo <123760949+Hakonovrebo@users.noreply.github.com>
2025-12-02 20:34:53 +0100

    solvd day 2

--- day1.py
+++ day1.py
@@ -1,23 +1,19 @@
-curent_pos = int
-"""
-input =
-L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82
-"""
-curent_pos = 50
+current_pos = 50
 counter = 0
-for l in input:
-  if l[0] == "L":
-    curent_pos= (curent_pos - int(l[1:]))%100 
-  else 
-    curent_pos= (curent_pos + int(l[1:]))%100 
-  if curent_pos == 0, counter += 1 
-print counter
+
+with open("input_1.txt", "r") as f:
+    for l in f:
+        l = l.strip()
+        if not l:
+            continue
+
+        direction = l[0]
+        amount = int(l[1:])
+        step = 1 if direction == "R" else -1
+
+        for _ in range(amount):
+            current_pos = (current_pos + step) % 100
+            if current_pos == 0:
+                counter += 1
+
+print(counter)
--- day2.py
+++ day2.py
@@ -1,1 +1,40 @@
+def is_double_repeat(num): 
+    if len(num)%2 != 0: #111 isn't a palindrom 
+        return False
+    return num[:len(num)//2] == num[len(num)//2:]
+        
+       
+def is_eny_part_repeting(num):
+    length = len(num)
+    divisorer = [d for d in range(2, length+1) if length % d == 0]
+    #print(divisorer)
+    for p in divisorer:
+        l2 = int(length/p)
+        
+        part = num[:l2]
+        p2 = ""
+        for _ in range(p):
+            p2 += part
+        if num == p2:
+            return True
+    return False
 
+is_eny_part_repeting("121212")
+
+f = open("input2.csv", "r") 
+input_data = f.read() 
+input = input_data.split(",") 
+counter = 0 
+counter2 = 0
+
+for x in input: 
+    start, stop = x.strip().split("-") 
+    for y in range(int(start), int(stop)+1): 
+        if is_double_repeat(str(y)): 
+            #print(y)
+            counter += y
+        if is_eny_part_repeting(str(y)):
+            counter2 += y
+
+print("part 1", counter)
+print("part 2 =", counter2)
