75272432f0551b37d9b15579ffd7f93cb9f09400
Håkon <123760949+Hakonovrebo@users.noreply.github.com>
2025-12-05 14:50:32 +0100

    Update day5.py to improve range handling
    Refactor input reading and processing logic.

--- day5.py
+++ day5.py
@@ -1,5 +1,4 @@
-"""
-3-5
+input_5 = """3-5
 10-14
 16-20
 12-18
@@ -9,15 +8,29 @@
 8
 11
 17
-32
-"""
+32"""
 
-fresh = set()
+fresh_ranges = []
 
-f = open("input_5.txt", "r")
-l = f.readline()
-while l =! "" and l.strip != "":
-    start, stop = l.strip().split("-")
-    fresh.update(range(start, stop+1)
-    l = f.readline()
+with open("input.txt") as f:
+    data = f.read().split("\n\n")
 
+first_block = data[0].splitlines()
+second_block = data[1].splitlines()
+
+for l in first_block:
+    start, stop = map(int, l.strip().split("-"))
+    fresh_ranges.append((start, stop)) 
+fresh_counter = []
+
+fresh = 0
+
+for v in second_block:
+    val = int(v)
+    for lo, hi in fresh_counter:
+        if lo <= val <= hi:
+            fresh += 1
+            break
+
+print(fresh)
+        
